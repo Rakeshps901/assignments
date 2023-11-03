b2c2b066209d9bae8c783bc344200d203294e553
Rakeshps901 <adithyaraj012003@gmail.com>
2023-11-03 13:14:14 +0530

    initial

--- lib/stg.dart
+++ lib/stg.dart
@@ -9,7 +9,7 @@ class stgridui extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Place"),),
+        appBar: AppBar(title: Text("Places"),),
         body:SingleChildScrollView(
           child: StaggeredGrid.count(crossAxisCount: 4,
             crossAxisSpacing: 10,
